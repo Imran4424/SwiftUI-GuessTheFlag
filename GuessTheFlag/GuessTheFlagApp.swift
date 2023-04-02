6a262009547357a564a91bc8cde8367bb3d3cfa3
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-02 22:49:51 +0600

    project init

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Shah Md Imran Hossain on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
